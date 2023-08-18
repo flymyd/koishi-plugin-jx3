de2861f8a8be53c5bc7fb9ba51d3d0cdd1aa3ad8
MYD\26637 <2663797538@qq.com>
2023-08-18 14:28:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# koishi-plugin-jx3
-jx3 bot for koishi
+# Koishi 模板仓库
+
+本仓库包含了 Koishi 的模板项目。
+
+## 使用教程
+
+<https://koishi.chat/manual/starter/boilerplate.html>
---
+++ cache/vite/deps/vue-i18n.js
@@ -0,0 +1,26 @@
+import {
+  DatetimeFormat,
+  I18nInjectionKey,
+  NumberFormat,
+  Translation,
+  VERSION,
+  castToVueI18n,
+  createI18n,
+  useI18n,
+  vTDirective
+} from "./chunk-MR54QHPG.js";
+import "./chunk-H5N6YIGP.js";
+import "./chunk-SHNSPNRG.js";
+import "./chunk-ZC22LKFR.js";
+export {
+  DatetimeFormat,
+  I18nInjectionKey,
+  NumberFormat,
+  Translation,
+  VERSION,
+  castToVueI18n,
+  createI18n,
+  useI18n,
+  vTDirective
+};
+//# sourceMappingURL=vue-i18n.js.map
